4cd2f25cda93d68b4d39420f8fd413a33649fd55
Peikii <peikiiwu@gmail.com>
2022-10-15 14:37:36 +0000

    Order model changed

--- service/models.py
+++ service/models.py
@@ -5,7 +5,7 @@ All of the models are stored in this module
 """
 import logging
 from flask_sqlalchemy import SQLAlchemy
-
+import json
 logger = logging.getLogger("flask.app")
 
 # Create the SQLAlchemy object to be initialized later in init_db()
@@ -14,30 +14,34 @@ db = SQLAlchemy()
 
 class DataValidationError(Exception):
     """ Used for an data validation errors when deserializing """
-
     pass
 
 
-class YourResourceModel(db.Model):
+class Order(db.Model):
     """
-    Class that represents a YourResourceModel
+    Class that represents a Order Model
+    id: int
+    items: list[int]
     """
 
     app = None
 
     # Table Schema
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(63))
+    user_id = db.Column(db.Integer, nullable=False)
+    create_time = db.Column(db.String(63), nullable=False)
+    status = db.Column(db.Integer, nullable=False, default=0)
+    items = db.Column(db.Text)
 
     def __repr__(self):
-        return "<YourResourceModel %r id=[%s]>" % (self.name, self.id)
+        return "<User {self.user_id} Order id=[{self.id}]>"
 
     def create(self):
         """
         Creates a YourResourceModel to the database
         """
-        logger.info("Creating %s", self.name)
-        self.id = None  # id must be none to generate next primary key
+        logger.info("Creating %s", self.id)
+        self.order_id = None  # id must be none to generate next primary key
         db.session.add(self)
         db.session.commit()
 
@@ -50,13 +54,13 @@ class YourResourceModel(db.Model):
 
     def delete(self):
         """ Removes a YourResourceModel from the data store """
-        logger.info("Deleting %s", self.name)
+        logger.info("Deleting %s", self.id)
         db.session.delete(self)
         db.session.commit()
 
     def serialize(self):
         """ Serializes a YourResourceModel into a dictionary """
-        return {"id": self.id, "name": self.name}
+        return {"id": self.id, "name": self.items}
 
     def deserialize(self, data):
         """
@@ -66,16 +70,16 @@ class YourResourceModel(db.Model):
             data (dict): A dictionary containing the resource data
         """
         try:
-            self.name = data["name"]
+            self.items = data["item"]
         except KeyError as error:
             raise DataValidationError(
                 "Invalid YourResourceModel: missing " + error.args[0]
-            )
+            ) from error
         except TypeError as error:
             raise DataValidationError(
                 "Invalid YourResourceModel: body of request contained bad or no data - "
                 "Error message: " + error
-            )
+            ) from error
         return self
 
     @classmethod
