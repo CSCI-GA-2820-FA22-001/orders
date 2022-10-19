346f0b6e71697306d202e922fdb62f2d7ad2bfb4
yz7043 <yz7043@nyu.edu>
2022-10-19 04:43:11 +0000

    fix test_create_order order id not correct bug

--- tests/test_models.py
+++ tests/test_models.py
@@ -64,10 +64,10 @@ class TestOrderModel(unittest.TestCase):
 		self.assertEqual(db_order.create_time, "2022-10-16")
 		self.assertEqual(db_order.status, 1)
 		# Test Items create
-		items = Items(order_id=123, item_id=321)
+		items = Items(order_id=order.id, item_id=321)
 		items.create()
 		self.assertTrue(items is not None)
-		self.assertEqual(items.order_id, 123)
+		self.assertEqual(items.order_id, order.id)
 		self.assertEqual(items.item_id, 321)
 
 		bad_order = Order.find(order.id+1)
