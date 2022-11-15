2195bdc0707ac916141abc29f51eef3b4578b776
yuxiangzhu <yzhubi@ust.hk>
2022-11-15 00:30:52 +0000

    add flake

--- service/__init__.py
+++ service/__init__.py
@@ -27,10 +27,10 @@ app.logger.info("  S E R V I C E   R U N N I N G  ".center(70, "*"))
 app.logger.info(70 * "*")
 
 try:
-    routes.init_db()  # make our SQLAlchemy tables
+	routes.init_db()  # make our SQLAlchemy tables
 except Exception as error:
-    app.logger.critical("%s: Cannot continue", error)
-    # gunicorn requires exit code 4 to stop spawning workers when they die
-    sys.exit(4)
+	app.logger.critical("%s: Cannot continue", error)
+	# gunicorn requires exit code 4 to stop spawning workers when they die
+	sys.exit(4)
 
 app.logger.info("Service initialized!")
