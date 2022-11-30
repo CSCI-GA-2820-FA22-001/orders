134022c277845e070f396f9ef0277d76dc34c59d
Derrick <derrickchang36@gmail.com>
2022-11-29 19:10:43 -0500

    add web UI

--- service/static/js/rest_api.js
+++ service/static/js/rest_api.js
@@ -5,26 +5,27 @@ $(function () {
     // ****************************************
 
     // Updates the form with data from the response
-    function update_form_data(res) {
-        $("#pet_id").val(res.id);
-        $("#pet_name").val(res.name);
-        $("#pet_category").val(res.category);
-        if (res.available == true) {
-            $("#pet_available").val("true");
-        } else {
-            $("#pet_available").val("false");
+    function update_form_data_order(res) {
+        $("#order_id").val(res.id)
+        $("#user_id").val(res.user_id);
+        $("#create_time").val(res.create_time);
+        $("#items").val(res.items);
+        if (res.status == 1) {
+            $("#status").val("created");
+        } else if (res.status == 2) {
+            $("#status").val("completed");
+        } else if (res.status == 3) {
+            $("#status").val("cancelled");
         }
-        $("#pet_gender").val(res.gender);
-        $("#pet_birthday").val(res.birthday);
     }
 
     /// Clears all form fields
-    function clear_form_data() {
-        $("#pet_name").val("");
-        $("#pet_category").val("");
-        $("#pet_available").val("");
-        $("#pet_gender").val("");
-        $("#pet_birthday").val("");
+    function clear_form_data_order() {
+        $("#order_id").val("");
+        $("#user_id").val("");
+        $("#create_time").val("");
+        $("#items").val("");
+        $("#status").val("");
     }
 
     // Updates the flash message area
@@ -59,7 +60,7 @@ $(function () {
             "items": items,
             "status": 1
         };
-
+        console.log(`data:` + JSON.stringify(data));
         $("#flash_message").empty();
         
         let ajax = $.ajax({
@@ -70,7 +71,7 @@ $(function () {
         });
 
         ajax.done(function(res){
-            update_form_data(res)
+            update_form_data_order(res)
             flash_message("Success")
         });
 
@@ -83,6 +84,73 @@ $(function () {
     // ****************************************
     // Order team code starts here
     // ****************************************
+    
+    // ****************************************
+    // Create a Order
+    // ****************************************
+
+    $("#create-order-btn").click(function () {
+        let user_id = parseInt($("#user_id").val());
+        let create_time = Math.floor(Date.now() / 1000);
+        let items = [];
+        if ($("#items").val() > 0) {
+            items = $("#items").val().split(",");
+        }
+
+        console.log(`User id: ${user_id}`);
+        console.log(`Item list: ${items}`);
+        let data = {
+            "user_id": user_id,
+            "create_time": create_time,
+            "items": items,
+            "status": 1
+        };
+        console.log(`data:` + JSON.stringify(data));
+
+        $("#flash_message").empty();
+        
+        let ajax = $.ajax({
+            type: "POST",
+            url: "/orders",
+            contentType: "application/json",
+            data: JSON.stringify(data),
+        });
+
+        ajax.done(function(res){
+            update_form_data_order(res)
+            flash_message("Success")
+        });
+
+        ajax.fail(function(res){
+            flash_message(res.responseJSON.message)
+        });
+    });
+
+    // ****************************************
+    // Read an order by order id
+    // ****************************************
+
+    $("#retrieve-order-btn").click(function () {
+        let order_id = parseInt($("#order_id").val());
+        console.log(`Order ID: ${order_id}`);
+        $("#flash_message").empty();
+        
+        let ajax = $.ajax({
+            type: "GET",
+            url: "/orders/" + order_id,
+        });
+
+        ajax.done(function(res){
+            update_form_data_order(res)
+            flash_message("Success")
+        });
+
+        ajax.fail(function(res){
+            flash_message(res.responseJSON.message)
+        });
+    });
+
+    
     
     // ****************************************
     // switch to retrieve page
