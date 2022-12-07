e520550e6c8c11d25fe27cefdd40b3313a737160
yz7043 <yz7043@nyu.edu>
2022-12-07 00:21:29 +0000

    bdd sample

---
+++ features/steps/order_steps.py
@@ -0,0 +1,17 @@
+import requests
+from behave import given
+from compare import expect
+
+@given('the following orders')
+def step_impl(context):
+	""" Delete all Pets and load new ones """
+	# List all of the pets and delete them one by one
+	rest_endpoint = f"{context.BASE_URL}/orders"
+	context.resp = requests.get(rest_endpoint, params={"user_id": 1})
+	expect(context.resp.status_code).to_equal(200)
+	for order in context.resp.json():
+		context.resp = requests.delete(f"{rest_endpoint}/{order['id']}")
+		expect(context.resp.status_code).to_equal(204)
+
+	# load the database with new pets
+	# TODO
